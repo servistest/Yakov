01182f2486b26ba3a1ebdb6e0dbe1a786f21d6ed
Alex K <servistest@rambler.ru>
2016-05-23 15:41:48 +0300

    exception all gut v1.0

--- Yakov/src/exception/BikeControl.java
+++ Yakov/src/exception/BikeControl.java
@@ -2,70 +2,42 @@ package exception;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.ParseException;
-
 import javax.swing.JOptionPane;
 
 public class BikeControl implements ActionListener{
 	private BikeForm bikeForm;
-	private static final Integer maxAist=100;
-	private static final Integer maxDamski=70;
-	private static final Integer maxOrlenok=50;
 	private Integer countBike;
+	private ValidateOrder validateOrder;
 	
 	 BikeControl(BikeForm bikeForm){
 		this.bikeForm=bikeForm;
 	}
-	 private void calculateOrder(){
+	 private void extractModel(){
 		 
 		 String nameBike=(String)bikeForm.getBike();
 		 if (nameBike!=null && !nameBike.equals("") && bikeForm.getCountBike()!=null){
-		
 			 try {
-				  countBike=Integer.parseInt(bikeForm.getCountBike());
+				  countBike=Integer.parseInt(bikeForm.getCountBike()); 
 			
 			 } catch (NumberFormatException e){
 				 System.out.println("Error input :" + e.getMessage());
-			 }
-			 
-	
-			 if (countBike>0){
-				 switch (nameBike) {
-				 	case "Aist": if(countBike<maxAist){
-				 		JOptionPane.showMessageDialog(null,"Order to accept bike "+ nameBike + " count =" +countBike );
-				 		}else {
-				 			JOptionPane.showMessageDialog(null,"Order not  to accept" );
-				 		}
-				 	break;
-				 	case "Damski": if(countBike<maxDamski){
-				 		JOptionPane.showMessageDialog(null,"Order to accept bike "+ nameBike + " count =" +countBike );
-				 		}else {
-				 			JOptionPane.showMessageDialog(null,"Order not to accept" );
-				 		}
-				 	break;
-				 	case "Orlenok": if(countBike<maxOrlenok){
-				 		JOptionPane.showMessageDialog(null,"Order to accept bike "+ nameBike + " count =" +countBike );
-				 		}else {
-				 			JOptionPane.showMessageDialog(null,"Order not  to accept" );
-				 		}
-					
-					break;
-
-				 	default:JOptionPane.showMessageDialog(null,"Order not to accept" );
-					break;
-				} 
-			 }
-			 
-			 
+			  }
+			 			 
+			 try {
+				 validateOrder=new ValidateOrder();
+				 validateOrder.calculateOrder(nameBike, countBike);
+			 }catch (TooManyBikesException e) {
+				JOptionPane.showMessageDialog(null,e.getMessage());
+			}
 		 }
-		 
+	
+
 	 }
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getActionCommand().equals("Order")){
-			calculateOrder();
-			//JOptionPane.showMessageDialog(null, "Press Order", "Order", JOptionPane.INFORMATION_MESSAGE);
+			extractModel();
 		}else{
 			System.out.println("Not Press Order");
 		}
--- Yakov/src/exception/ValidateOrder.java
+++ Yakov/src/exception/ValidateOrder.java
@@ -3,44 +3,34 @@ package exception;
 import javax.swing.JOptionPane;
 
 public class ValidateOrder {
-	private BikeForm bikeForm;
+	
 	private static final Integer maxAist=100;
 	private static final Integer maxDamski=70;
 	private static final Integer maxOrlenok=50;
-	private Integer countBike;
+
 	
-	public void calculateOrder() throws TooManyBikesException{
-		 
-		 String nameBike=(String)bikeForm.getBike();
-		 if (nameBike!=null && !nameBike.equals("") && bikeForm.getCountBike()!=null){
+	public void calculateOrder(String nameBike,Integer countBike) throws TooManyBikesException{
 		
-			 try {
-				  countBike=Integer.parseInt(bikeForm.getCountBike());
-			
-			 } catch (NumberFormatException e){
-				 //System.out.println("Error input :" + e.getMessage());
-				 throw new TooManyBikesException("Can not ship " + nameBike + " count =" +countBike );
-			 }
-			 
-	
-			 if (countBike>0){
+		if(nameBike!=null && countBike!=null){
+			if (countBike>0){
 				 switch (nameBike) {
 				 	case "Aist": if(countBike<maxAist){
 				 		JOptionPane.showMessageDialog(null,"Order to accept bike "+ nameBike + " count =" +countBike );
 				 		}else {
-				 			JOptionPane.showMessageDialog(null,"Order not  to accept" );
+				 			throw new TooManyBikesException("Order not accept bike "+ nameBike + ". Very many bikes =" +countBike );
+				 			
 				 		}
 				 	break;
 				 	case "Damski": if(countBike<maxDamski){
 				 		JOptionPane.showMessageDialog(null,"Order to accept bike "+ nameBike + " count =" +countBike );
 				 		}else {
-				 			JOptionPane.showMessageDialog(null,"Order not to accept" );
+				 			throw new TooManyBikesException("Order not accept bike "+ nameBike + ". Very many bikes =" +countBike );
 				 		}
 				 	break;
 				 	case "Orlenok": if(countBike<maxOrlenok){
 				 		JOptionPane.showMessageDialog(null,"Order to accept bike "+ nameBike + " count =" +countBike );
 				 		}else {
-				 			JOptionPane.showMessageDialog(null,"Order not  to accept" );
+				 			throw new TooManyBikesException("Order not accept bike "+ nameBike + ". Very many bikes =" +countBike );
 				 		}
 					
 					break;
@@ -54,6 +44,10 @@ public class ValidateOrder {
 		 }
 		 
 	 }
-
-
+				
 }
+	
+			 
+
+
+
