f9350fa16d5e6dfb5e8bcb1f039136eab5a9b404
Alex K <servistest@rambler.ru>
2016-05-24 14:06:39 +0300

    d

--- Blackberry.java
+++
@@ -1,8 +0,0 @@
-
-public class Blackberry {
-	// I'm a Blackberry phone. Put me in the Box please.
-	
-	public Blackberry(){
-		System.out.println("I'm Blackberry");
-	}
-}
--- Nokia.java
+++
@@ -1,9 +0,0 @@
-
-public class Nokia {
-   // I'm a Nokia phone. Put me in the Box please.
-	
-	public Nokia(){
-		System.out.println("I'm Nokia");
-	}
-	
-}
