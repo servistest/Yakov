b518ef009924d652947879221cca399762a83909
Alex K <servistest@rambler.ru>
2016-05-28 08:37:31 +0300

    1

--- Yakov/src/exception/TooManyBikesException.java
+++ Yakov/src/exception/TooManyBikesException.java
@@ -5,11 +5,5 @@ package exception;
 	 public TooManyBikesException(String message) {
 		super(message);
 	}
-=======
-public class TooManyBikesException extends Exception {
-	public TooManyBikesException(String message) {
-		super(message);
-	}
 
->>>>>>> refs/heads/master
-}
+
