3bd2af4d4033cd7678e2170bd07d51496284b4fa
Alex K <servistest@rambler.ru>
2016-05-29 12:37:48 +0300

    change Transaction

--- Yakov/src/jdbc/Transaction.java
+++ Yakov/src/jdbc/Transaction.java
@@ -9,22 +9,12 @@ import java.sql.Date;
 
 public class Transaction {
 	
-	private static PreparedStatement createPrepareStatement(Connection con) throws SQLException {
-		String sql=("SELECT * FROM STUDENTS WHERE firstName=? or dateOfBirth>?");
-		PreparedStatement prs=con.prepareStatement(sql);
-		prs.addBatch("insert into students values(25,'Ivan3','Stepanov3','Sergevich','1990-05-03','M',2,2007)");
-		prs.addBatch("insert into students values(26,'Ivan3','Stepanov3','Sergevich','1990-05-03','M',2,2007)");
-		stm.addBatch("insert into students values(27,'Ivan3','Stepanov3','Sergevich','1990-05-03','M',2,2007)");
-		return prs;
-		
-	}
-
+	
 	public static void main(String[] args) throws SQLException {
 	
 		try(
 			Connection	con= DriverManager.getConnection("jdbc:mysql://localhost:3306/students","root","root");
-				
-				 	
+	 	
 		){
 			//http://stackoverflow.com/questions/9260159/java-7-automatic-resource-management-jdbc-try-with-resources-statement
 			con.setAutoCommit(false);
