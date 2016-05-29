3e122652d37ccf5954d7c43d4fa2adf44e7c344f
Alex K <servistest@rambler.ru>
2016-05-29 12:33:58 +0300

    add prepereSt

--- Yakov/src/jdbc/Transaction.java
+++ Yakov/src/jdbc/Transaction.java
@@ -2,8 +2,8 @@ package jdbc;
 
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.Date;
 
@@ -12,37 +12,40 @@ public class Transaction {
 	private static PreparedStatement createPrepareStatement(Connection con) throws SQLException {
 		String sql=("SELECT * FROM STUDENTS WHERE firstName=? or dateOfBirth>?");
 		PreparedStatement prs=con.prepareStatement(sql);
-		prs.setString(1, "Иван");
-		prs.setDate(2, Date.valueOf("1991-03-20"));
+		prs.addBatch("insert into students values(25,'Ivan3','Stepanov3','Sergevich','1990-05-03','M',2,2007)");
+		prs.addBatch("insert into students values(26,'Ivan3','Stepanov3','Sergevich','1990-05-03','M',2,2007)");
+		stm.addBatch("insert into students values(27,'Ivan3','Stepanov3','Sergevich','1990-05-03','M',2,2007)");
 		return prs;
 		
 	}
 
 	public static void main(String[] args) throws SQLException {
 	
-		
 		try(
 			Connection	con= DriverManager.getConnection("jdbc:mysql://localhost:3306/students","root","root");
-			PreparedStatement prs=createPrepareStatement(con);
+				
 				 	
 		){
 			//http://stackoverflow.com/questions/9260159/java-7-automatic-resource-management-jdbc-try-with-resources-statement
 			con.setAutoCommit(false);
-			prs.addBatch("insert into students values(22,'Ivan3','Stepanov3','Sergevich','1990-05-03','M',2,2007)");
-			prs.addBatch("insert into students values(23,'Ivan3','Stepanov3','Sergevich','1990-05-03','M',2,2007)");
-			prs.addBatch("insert into students values(24,'Ivan3','Stepanov3','Sergevich','1990-05-03','M',2,2007)");
-			prs.executeBatch();
+			try(Statement stm=con.createStatement()){		
+				stm.addBatch("insert into students values(25,'Ivan3','Stepanov3','Sergevich','1990-05-03','M',2,2007)");
+				stm.addBatch("insert into students values(26,'Ivan3','Stepanov3','Sergevich','1990-05-03','M',2,2007)");
+				stm.addBatch("insert into students values(27,'Ivan3','Stepanov3','Sergevich','1990-05-03','M',2,2007)");
+				stm.executeBatch();
+				
+			}catch (SQLException e){
+				con.rollback();	
+				System.out.println("Rollback transaction:" + e.getMessage());
+			}
 			con.commit();
-			
-			
-		}catch (SQLException e){
-			//con.rollback();
-			System.out.println(e.getMessage());
+			con.setAutoCommit(true);
 		}
-			
+
+					
 	}
 		
-	}
+}
 
 
 
